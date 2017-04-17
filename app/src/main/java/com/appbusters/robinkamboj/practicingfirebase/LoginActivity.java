b100bcb4f09a1ce5ac1f3d5e4f3427fb2d85c794
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-04-17 14:43:08 +0530

    working on login.

--- app/src/main/java/com/appbusters/robinkamboj/practicingfirebase/LoginActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/practicingfirebase/LoginActivity.java
@@ -3,11 +3,42 @@ package com.appbusters.robinkamboj.practicingfirebase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private LoginButton facebook_sign_in;
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        callbackManager = CallbackManager.Factory.create();
+
+        facebook_sign_in = (LoginButton) findViewById(R.id.facebook_sign_in);
+        facebook_sign_in.setReadPermissions("public_profile");
+        facebook_sign_in.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
     }
 }
