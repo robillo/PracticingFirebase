12c265f81a1094571d7fbdd5c81c0c1a9f521be2
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-04-17 14:45:06 +0530

    ...

--- app/src/main/java/com/appbusters/robinkamboj/practicingfirebase/LoginActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/practicingfirebase/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.appbusters.robinkamboj.practicingfirebase;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -39,6 +40,11 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
