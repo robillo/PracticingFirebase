8eb4255d57d854f919cd3fa086b91e2009a651a4
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-04-25 19:12:53 +0530

    User for DS.

--- app/src/main/java/com/appbusters/robinkamboj/practicingfirebase/view/activities/MainActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/practicingfirebase/view/activities/MainActivity.java
@@ -1,11 +1,20 @@
 package com.appbusters.robinkamboj.practicingfirebase.view.activities;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.appbusters.robinkamboj.practicingfirebase.R;
 import com.appbusters.robinkamboj.practicingfirebase.model.User;
+import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -16,6 +25,9 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
 
+import butterknife.BindView;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
 
     private DatabaseReference mDatabase;
@@ -24,11 +36,31 @@ public class MainActivity extends AppCompatActivity {
 
     private HashMap<String, HashMap<String, Boolean>> allInterests;
     private HashMap<String, Boolean> mot, rel, ast, yog, ayu, hea, die;
+    private static final int RESULT_LOAD_IMAGE = 8008;
+    private ImageButton userProfilePic;
+
+//    @BindView(R.id.user_profile_photo)
+//    ImageButton userProfilePhoto;
+//    @BindView(R.id.header_cover_image)
+//    ImageButton userCoverPhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userProfilePic = (ImageButton) findViewById(R.id.user_profile_photo);
+
+        userProfilePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.e("Insert Requested", "YES!.");
+                Intent i = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+            }
+        });
 
         mDatabase = FirebaseDatabase.getInstance().getReference("users");
         mFirebaseAuth = FirebaseAuth.getInstance();
@@ -123,5 +155,29 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            Glide.with(this)
+                    .load(picturePath)
+                    .into(userProfilePic);
+
+        }
+
+
     }
 }
